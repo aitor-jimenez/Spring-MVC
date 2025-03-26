606ecf38438873b0b10f5c7b8df9a5a1ca5772be
Aitor Jiménez Segura <aitor.jimenez@babelgroup.com>
2025-03-26 09:25:47 +0100

    Preparación controlador de clientes

--- SpringMVCDemo/src/main/java/es/neesis/mvcdemo/controller/ClienteController.java
+++ SpringMVCDemo/src/main/java/es/neesis/mvcdemo/controller/ClienteController.java
@@ -1,4 +1,53 @@
 package es.neesis.mvcdemo.controller;
 
+import es.neesis.mvcdemo.modelos.Cliente;
+import es.neesis.mvcdemo.servicios.ServicioCliente;
+import es.neesis.mvcdemo.servicios.ServicioSucursal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+
+@Controller
+@RequestMapping("/clientes")
 public class ClienteController {
+
+    private ServicioCliente servicioCliente;
+    private ServicioSucursal servicioSucursal;
+
+    public ClienteController(ServicioCliente servicioCliente, ServicioSucursal servicioSucursal) {
+        this.servicioCliente = servicioCliente;
+        this.servicioSucursal = servicioSucursal;
+    }
+
+    @GetMapping
+    public String listarClientes(Model model) {
+        model.addAttribute("clientes", this.servicioCliente.listarClientes());
+        model.addAttribute("sucursales", this.servicioSucursal.listarSucursales());
+        return "/clientes";
+    }
+
+    @PostMapping
+    public String addCliente(Cliente cliente, Model model) {
+        this.servicioCliente.altaCliente(cliente);
+        return "/clientes";
+    }
+
+    @GetMapping("/{dni}")
+    public String getCliente(@RequestParam String dni, Model model) {
+        model.addAttribute("cliente", this.servicioCliente.buscarCliente(dni));
+        return "detalleCliente";
+    }
+
+    @PutMapping("/{dni}")
+    public String modificarCliente(@PathVariable String dni, Cliente cliente, Model model) {
+        this.servicioCliente.modificarCliente(cliente);
+        return "/clientes";
+    }
+
+    @PostMapping("/{dni}")
+    public String borrarCliente(@RequestParam String dni, Model model) {
+        this.servicioCliente.bajaCliente(dni);
+        return "/clientes";
+    }
+
 }
--- SpringMVCDemo/src/main/java/es/neesis/mvcdemo/modelos/Cliente.java
+++ SpringMVCDemo/src/main/java/es/neesis/mvcdemo/modelos/Cliente.java
@@ -1,4 +1,47 @@
 package es.neesis.mvcdemo.modelos;
 
+import org.springframework.stereotype.Component;
+
 public class Cliente {
+
+    private String dni;
+    private String nombre;
+    private String direccion_postal;
+    private String email;
+    private String telefono;
+    private Sucursal sucursal_principal;
+
+    public Cliente(String dni, String nombre, String direccion_postal, String email, String telefono, Sucursal sucursal_principal) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.direccion_postal = direccion_postal;
+        this.email = email;
+        this.telefono = telefono;
+        this.sucursal_principal = sucursal_principal;
+    }
+
+    public String getDni() {
+        return this.dni;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String getDireccion_postal() {
+        return this.direccion_postal;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getTelefono() {
+        return this.telefono;
+    }
+
+    public Sucursal getSucursal_principal() {
+        return this.sucursal_principal;
+    }
+
 }
